dd9695ae9fb6d1089bed047490dfba07c06d22bd
Daniel Fitzpatrick <dfitzpatrick@panoston.com>
2019-11-11 15:05:25 -0600

    v 0.0.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject gooff "0.1.0-SNAPSHOT"
+(defproject gooff "0.0.1"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
